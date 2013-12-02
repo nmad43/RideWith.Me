8d67607b19ab80c4775d4ba607139504d1ff73aa
Erik Edrosa <erik.edrosa@gmail.com>
2013-12-02 13:47:42 -0500

    Add User entity and Endpoint

---
+++ src/com/g5/ridewithme/User.java
@@ -0,0 +1,61 @@
+package com.g5.ridewithme;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String location;
+	private String car;
+	
+	public User() {
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public String getCar() {
+		return car;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public void setCar(String car) {
+		this.car = car;
+	}
+}
